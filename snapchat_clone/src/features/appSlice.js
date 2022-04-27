3182051b5057b4e1e7eaf66a773fa9a114c293ca
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-04-27 14:20:23 +0100

    cleanup and react-webcam install

---
+++ snapchat_clone/src/features/appSlice.js
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+  
+  
+});
+
+export const { incrementByAmount } = appSlice.actions;
+
+
+export const selectapp = (state) => state.app.value;
+
+
+export default appSlice.reducer;
---
+++ snapchat_clone/src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCujXdPKxwUuk2YIeHLD8vUXnfbQJkxlvU",
+  authDomain: "snapchat-clone-dad9e.firebaseapp.com",
+  projectId: "snapchat-clone-dad9e",
+  storageBucket: "snapchat-clone-dad9e.appspot.com",
+  messagingSenderId: "463724307529",
+  appId: "1:463724307529:web:8b8c589ba8dc03879a8b98",
+  measurementId: "G-X5KHBPML68",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, storage, provider };
