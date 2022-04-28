444134fd8b2a40f66485afd9d78c00fdbaba9889
Brijesh Pujara <pujara.brijesh1@gmail.com>
2022-04-28 14:12:22 +0100

    chats route added to app js

--- snapchat_clone/src/App.js
+++ snapchat_clone/src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 import WebcamCapture from "./components/WebcamCapture";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Preview from "./components/Preview";
-
+import Chats from "./components/Chats";
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <div className="app-body">
+        <Routes>
+            <Route path="/chats" element={<Chats />} />
+          </Routes>
         <Routes>
             <Route path="/preview" element={<Preview />} />
           </Routes>
